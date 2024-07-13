ead60186ff2234d5f8705c9ef1bfb62c5e68e626
Salawat9048 <salawat9048@outlook.com>
2024-07-13 16:25:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+from typing import List, Any
+def get_matrix(n, m, value):
+    matrix: list[list[Any]] = []
+    for i in range(n):
+        row = []
+        for j in range (m):
+            row.append(value)
+        matrix.append(row)
+    return matrix
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
+print(result1)
+print(result2)
+print(result3)
